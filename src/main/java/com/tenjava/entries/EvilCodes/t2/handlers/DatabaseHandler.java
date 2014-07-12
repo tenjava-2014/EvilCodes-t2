7baadb825359e54b568c671895c920eb104631bc
Paul : EvilCodes <evilcodes.dev@gmail.com>
2014-07-12 13:03:57 +0200

    Added config and messages file

---
+++ src/main/java/com/tenjava/entries/EvilCodes/t2/handlers/DatabaseHandler.java
@@ -0,0 +1,10 @@
+package com.tenjava.entries.EvilCodes.t2.handlers;
+
+import org.bukkit.entity.Player;
+
+public class DatabaseHandler {
+
+    public static boolean userExists(final Player player) {
+
+    }
+}
